500d64a2cfaa93aef3748f9d53586dff632f8ed4
kshitij <135230935+kchat3103@users.noreply.github.com>
2024-03-09 01:12:35 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }, 
+    }, {timestamps:true} // to save time of creation of users and time of update
+);
+
+const User = mongoose.model('User',userSchema); //create model
+
+export default User; // can use this model later during sign up when we create a user or perform updates
