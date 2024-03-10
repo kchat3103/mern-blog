d8285cc7451174cc0a1fc8c691bbaf9bd2e8a7b9
kshitij <135230935+kchat3103@users.noreply.github.com>
2024-03-11 01:20:30 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next)=>{  //async as we need for mongoDB to authenticate first and then allow access
     const {username, email, password} = req.body;
@@ -23,5 +24,32 @@ export const signup = async (req,res,next)=>{  //async as we need for mongoDB to
     } catch (error) {
         next(error);
     }
+};
 
-}
+export const signin = async(req,res,next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password || email ==='' || password===''){
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400,'Invalid Password'));
+        }
+        const token = jwt.sign(
+            {id: validUser._id}, process.env.JWT_SECRET);//session expires when user closes the browser
+            const {password:_pass, ...rest}= validUser._doc;
+
+            res.status(200).cookie('access_token',token,{ //sent access token  to cookie
+                httpOnly:true
+            }).json(rest); //send this back to user given by validUser
+    } catch (error) {
+        next(error);
+    }
+};
