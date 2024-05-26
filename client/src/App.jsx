cb30c56647f5d9aca3f3aa2526dce532de3dfe8c
kshitij <135230935+kchat3103@users.noreply.github.com>
2024-05-27 00:59:39 +0400

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import DashBoard from "./pages/DashBoard"
-import Projects from "./pages/Projects"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard.jsx';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/> {/* So that header is visible in all pages*/}
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/dashboard" element={<DashBoard/>} />
-        <Route path="/projects" element={<Projects/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route path='/projects' element={<Projects />} />
       </Routes>
       <Footer />
-    
     </BrowserRouter>
-  )
+  );
 }
